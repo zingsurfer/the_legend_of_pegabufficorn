6e0a97a9e94419e8e0843bed77eb11ecc8fc1d73
Autumn-Martin <autumnmartin.dev@gmail.com>
2020-02-16 00:35:01 -0700

    Direct app root rendering

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('src'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/src/app.html'));
+});
+
+app.listen(port, () => console.log(`Pegabufficorn is listening on port ${port}!`))
