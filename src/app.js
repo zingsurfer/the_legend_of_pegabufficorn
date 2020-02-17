ed2e670cc3b97e4056aecf5a3888599d6ddac590
Autumn-Martin <autumnmartin.dev@gmail.com>
2020-02-16 18:34:17 -0700

    Add physics to pegabufficorn & sky background

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 var config = {
     width: 800,
-    type: Phaser.AUTO,
     height: 700,
     physics: {
       default: 'arcade',
@@ -12,15 +11,24 @@ var config = {
     scene: {
         preload: preload,
         create: create
-    }
+    },
+    type: Phaser.AUTO
 };
 
 var game = new Phaser.Game(config);
 
 function preload () {
+  this.load.image('sky', 'assets/sky.png');
   this.load.image('pegabufficorn', 'assets/pegabufficorn.png');
 }
 
 function create () {
-  this.add.image(350, 350, 'pegabufficorn')
+  this.add.image(400, 350, 'sky');
+  var platforms = this.physics.add.staticGroup();
+
+  var pegabufficorn = this.physics.add.sprite(400, 0, 'pegabufficorn');
+  pegabufficorn.setBounce(0.2);
+  pegabufficorn.setCollideWorldBounds(true);
+
+  this.physics.add.collider(pegabufficorn, platforms);
 }
